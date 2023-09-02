3bd41c4708156211618f2a27c4657ff1bda1730f
= <=>
2023-09-02 10:23:13 +0530

    Showing Error

--- Controller/expense.js
+++ Controller/expense.js
@@ -60,6 +60,7 @@ exports.postAddExpense=async (req,res,next)=>{
     }
     catch(err)
     {   
+        console.log(err)
         await transaction.rollback();
         res.status(500).json({error:"Something went wrong"})
     }
